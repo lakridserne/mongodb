46f02025e6a74627aca100b3d28ee4bb2cbfee07
Gordon <gordonmikkelsen87@gmail.com>
2020-05-17 21:28:29 +0200

    Update with finishing changes

--- script.js
+++ script.js
@@ -1,40 +1,19 @@
 
-// db.tweets.mapReduce(
-//     //map function
-//     function() 
-//     {
-        
-//             //var hashtags = this.entities.hashtags;
-//             this.entities.hashtags.forEach( tweet => emit(tweet.text, 1));
-        
-//     },  
-//     //reduce function
-//     function(key,values) 
-//     {   
-      
-//         return Array.sum(values);
-       
-//     }, 
-//     {  
-//        out: "top_10_hashtags"
-//     }
-//  ).find().sort({'value': -1}).limit(10);
-
- db.tweets.mapReduce(
-    // Map function
+// Map reduce assignment
+db.tweets.mapReduce(
+    // functions to be used to count the occourances of a specific hashtag
     function () {
-        // Iterate all hashtags and emit text/ tag as key and add a 1 to the array for each occurance
-        this.entities.hashtags.forEach(t => emit(t.text, 1));
+        this.entities.hashtags.forEach(tag => emit(tag.text, 1));
     },
     // Reduce function
     function (key, values) {
-        // Return length of array(1's / count of hashtag)
         return values.length
     },
     {
         out: "top_10_tweets",
-        // only returns rows with hashtags
+        // filter so we only get hashtags
         query: { "entities.hashtags": { $exists: true } }
     }
-    // returns student list and sorts desc and returns first 10;
-).find().sort({ 'value': -1 }).limit(10)
+    // sort the tweets descending
+).find().sort({value: -1}).limit(10);
+
